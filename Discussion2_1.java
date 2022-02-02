e6581a6b7f89e0b39de95a695c458b0800853439
Michael <89663481+MichaelSousa00@users.noreply.github.com>
2022-02-02 16:01:35 -0500

    Add files via upload

---
+++ Discussion2_1.java
@@ -0,0 +1,36 @@
+class Phone{
+    String color;
+    int batteryLife;
+    boolean mobile;
+    int ram;
+    int storage;
+}
+class SmartPhone extends Phone{
+    boolean wifiConnection;
+    boolean touchScreen;
+    String operatingSystem;
+}
+class IPhone extends SmartPhone{
+    boolean facetime;
+}
+
+public class Discussion2_1{
+    public static void main(String[] args){
+        IPhone myPhone = new IPhone(); //new iphone object
+
+        myPhone.color = "grey";
+        myPhone.batteryLife = 100;
+        myPhone.wifiConnection = true;
+        myPhone.mobile = true;
+        myPhone.ram = 12;
+        myPhone.facetime=true;
+        myPhone.storage = 1;
+        myPhone.operatingSystem = "IOS";
+
+        System.out.println("My phones color is " +myPhone.color);
+        System.out.println("My phones OS is "+myPhone.operatingSystem);
+        System.out.println("My phones ram and storage is "+myPhone.ram+ " gigabytes and " +myPhone.storage+" terabyte");
+        System.out.println("My phones battery life is at "+myPhone.batteryLife);
+        System.out.println("Does my phone have facetime? "+myPhone.mobile);
+    }
+}
